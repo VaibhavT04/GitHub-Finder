72e0ce2f6473a4e6a1f12ff6ec287efeb444167c
Vaibhav <vaibhavtatkare04@gmail.com>
2024-10-04 01:24:23 +0530

    Creted context and shifted states and functions to it

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,28 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useContext } from "react"
 import Spinner from "../layout/Spinner"
+import UserItem from "./UserItem"
+import GithubContext from "../../context/github/GithubContext"
 
 
 function UserResults() {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
+  const {users, loading, fetchUsers} = useContext(GithubContext)
 
   useEffect(() => {
     fetchUsers()
   }, [])
-
-  const fetchUsers = async () => {
-    const response = await fetch(`https://api.github.com/users`,{
-        headers: {
-            Authorization: `token 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`
-        }
-    })
-    const data = await response.json()
-    console.log(data)
-    setUsers(data);
-    setLoading(false)
-  }
-
 
 
   if(!loading){
@@ -30,13 +17,13 @@ function UserResults() {
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 
         lg:grid-cols-2 md:grid-cols-2">
              {users.map((user)=>(
-                <h3>{user.login}</h3>
+                <UserItem key={user.id} user={user}/>
              ))}
         </div>
       )
   }
   else{
-    return <h3>Loading...</h3>
+    return <Spinner/>
   }
 }
 
